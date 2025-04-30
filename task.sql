57ce87924df9866b64b78d412c3720f02a7d2e18
Taras Savchyn <savik1992@gmail.com>
2025-04-30 23:02:48 +0300

    update task.sql

--- task.sql
+++ task.sql
@@ -1,12 +1,11 @@
-COMMIT;
 USE ShopDB;
-
-START TRANSACTION;
 
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
 
 SET @order_id = LAST_INSERT_ID();
+
+START TRANSACTION;
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
 VALUES (@order_id, 1, 1);
